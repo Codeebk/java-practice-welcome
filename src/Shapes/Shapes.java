3c5c12802aef77b81fe51732a241adebcf2bca31
Codee King <codee.king@trintech.com>
2019-11-11 09:57:07 -0600

    printing shapes

---
+++ src/Shapes/Shapes.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Shapes;
+import java.util.Scanner;
+
+/**
+ * @author codeeking
+ *
+ */
+public class Shapes {
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		
+		System.out.printf("%n*********%n*       *%n*       *%n*********");
+
+	} // end method main
+
+} //end class Shapes
--- src/Welcome1/Welcome1.java
+++ src/Welcome1/Welcome1.java
@@ -14,14 +14,14 @@ public class Welcome1 {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter first integer: ");
-//		int number1 = input.nextInt();
-//		
-//		System.out.print("Enter Second integer: ");
-//		int number2 = input.nextInt();
-//		
-//		int sum = number1 + number2;
-//		
-//		System.out.printf("sum is %d%n", sum);
+		int number1 = input.nextInt();
+		
+		System.out.print("Enter Second integer: ");
+		int number2 = input.nextInt();
+		
+		int sum = number1 + number2;
+		
+		System.out.printf("sum is %d%n", sum);
 
 
 	} // end method main
